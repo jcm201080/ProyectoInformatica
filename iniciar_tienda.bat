265a4d86e2bef5c540f3962ac55a6cd6fd9a80f5
Jesus <jcm201080@hotmail.com>
2026-01-13 10:14:27 +0100

    Cambios o par meter en ssh

---
+++ iniciar_tienda.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d "C:\Users\JCM\PycharmProjects\ProyectoInformatica"
+"C:\Users\JCM\PycharmProjects\ProyectoInformatica\.venv\Scripts\python.exe" main.py
+start http://127.0.0.1:10000
+pause
