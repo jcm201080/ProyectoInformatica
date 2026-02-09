aef755279a658bc2db2b49481593b63443d61bbc
Jesus <jcm201080@hotmail.com>
2026-02-09 13:09:40 +0100

    Modificación bd

--- db.py
+++ db.py
@@ -14,7 +14,7 @@ import os
 from sqlalchemy import create_engine
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, "database", "productos.db")
+DB_PATH = os.path.join(BASE_DIR, "database", "tu_base_de_datos.db")
 
 engine = create_engine(
     f"sqlite:///{DB_PATH}",
